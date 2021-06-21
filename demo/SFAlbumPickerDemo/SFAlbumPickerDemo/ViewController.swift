7c6736ad9a6c0f68062746a1043da6cd60deb7dc
AbnormalProgrammer <273433580@qq.com>
2021-06-21 09:59:31 +0800

    第一次提交

---
+++ demo/SFAlbumPickerDemo/SFAlbumPickerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SFAlbumPickerDemo
+//
+//  Created by Stroman on 2021/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemBlue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
